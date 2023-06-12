fd2ac471be27dd7a427bf22acb59cd217639016d
Richard Huang <richardhuang.huang@gmail.com>
2023-06-12 13:29:58 -0700

    [pastry] Add tag and generate shell completions

--- Formula/pastry.rb
+++ Formula/pastry.rb
@@ -1,8 +1,7 @@
 class Pastry < Formula
   desc "Sweet command-line tool to interact with multiple pastebin backends"
   homepage "https://github.com/aeolyus/pastry"
-  url "https://github.com/aeolyus/pastry.git"
-  version "0.1.0"
+  url "https://github.com/aeolyus/pastry.git", tag: "v0.1.0"
   license "MPL-2.0"
   head "https://github.com/aeolyus/pastry.git", branch: "master"
 
@@ -11,6 +10,7 @@ class Pastry < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
+    generate_completions_from_executable(bin/"pastry", "completion", shell_parameter_format: :arg)
   end
 
   test do
